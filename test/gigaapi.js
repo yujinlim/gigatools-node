e60b042b54a8907a3d6bfc9f85262e39b705b629
yujinlim <adamlyj89@gmail.com>
2014-12-26 16:47:23 +0800

    ignore data created from cli and remove unwanted test cases

--- test/gigaapi.js
+++ test/gigaapi.js
@@ -1,52 +1,26 @@
 'use strict';
 var chai = require('chai');
-var proxyquire = require('proxyquire');
+var proxyquire = require('proxyquire').noPreserveCache();
 var bluebird = require('bluebird');
 var expect = chai.expect;
 
-describe('Gigaapi', function() {
-  var gigaApi;
-  before(function() {
-    gigaApi = require('../lib/gigaapi');
-  });
-
-  it('should be define', function() {
-    expect(gigaApi).to.not.be.undefined;
-  });
-
-});
-
+// Unit Testing
 describe('Gigaapi#country', function() {
   var requestStub = {};
   var countries = ['Argentina'];
   var apiKey = '123';
   var gigaApi;
 
-  it('should have called with appropriate data', function() {
+  it('should have called with appropriate data', function(done) {
     proxyquire.noCallThru();
     gigaApi = proxyquire('../lib/gigaapi', {
       request: function(data) {
         expect(data.uri).to.not.be.undefined;
         expect(data.qs).to.be.a('object');
         expect(data.qs['countries[]']).to.eql(countries);
-      }
-    });
-
-    gigaApi(apiKey).country(countries);
-  });
-
-  describe('Gigaapi#country#validations', function(done) {
-    proxyquire.noCallThru();
-    
-    var request = function(){
-      return new bluebird(function() {
-        return [];
         done();
-      });
-    };
-
-    gigaApi = proxyquire('../lib/gigaapi', {
-      request: request
+        return bluebird.resolve([])
+      }
     });
 
     gigaApi(apiKey).country(countries);
